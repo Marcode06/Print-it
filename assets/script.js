df17461b5de0d893d52a2f21c59a59668009d35e
Marc <marc.sonaglia@gmail.com>
2024-07-09 14:25:51 +0200

    ajout event listener

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,20 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+//récuparation des id des flêches
+let arrowLeft = document.querySelector('.arrow_left');
+let arrowRight = document.querySelector('.arrow_right');
+
+//ajoute un écouteur click et l'affiche dans la console si on clique
+arrowLeft.addEventListener('click', function() {
+	console.log('je clique sur La flèche gauche');
+})
+
+arrowRight.addEventListener('click', function() {
+	console.log('je clique sur La flèche droite');
+})
+
+
+
+
